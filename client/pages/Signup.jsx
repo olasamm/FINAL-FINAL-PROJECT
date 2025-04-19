091f43378f627e203e427dfad72989312d178b0d
hghest <gbadebosamuel23@gmail.com>
2025-04-19 08:44:08 -0700

    done

--- client/pages/Signup.jsx
+++ client/pages/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
         const allValue = {name, mail, password} 
 
         console.log(allValue)
-        const url = 'https://final-final-project-2.onrender.com/submit/signup'
+        const url = 'https://final-final-project-2.onrender.com/submit/'
         axios.post(url, allValue)
         .then((res) => {
           console.log(res)
