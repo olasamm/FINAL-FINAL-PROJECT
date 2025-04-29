70c71087558d569b44f953a9d6387a35b2b8a7b1
olasamm <gbadebosamuel23@gmail.com>
2025-04-29 18:25:44 +0100

    done

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './Navnar.css';
-import { Link } from 'react-router-dom'
-
-
+import React from 'react';
+import './Navbars.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark Navabr">
+      <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
           <a className="navbar-brand text-light" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -19,12 +25,20 @@ const Navbar = () => {
               <a className="nav-link text-light" href="#">About Us</a>
               <a className="nav-link text-light" href="#">Contact Us</a>
               <li className="nav-item dropdown">
-                <button className="btn btn-light dropdown-toggle portal mx-5" data-bs-toggle="dropdown" aria-expanded="false">
+                <button
+                  className="btn btn-light dropdown-toggle portal mx-5"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   E-portal
                 </button>
                 <ul className="dropdown-menu dropdown-menu-light border-0">
-                  <Link className="dropdown-item portal" to="/signup">Lecturer</Link>
-                 <Link className="dropdown-item portal" to="/signup">Student</Link>
+                  <li>
+                    <Link className="dropdown-item portal" to="/signup">Lecturer</Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item portal" to="/signup">Student</Link>
+                  </li>
                 </ul>
               </li>
             </div>
@@ -32,7 +46,7 @@ const Navbar = () => {
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
